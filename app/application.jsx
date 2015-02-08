b01e2f2a814d7d09890ab0f170db7fc13cbdcde3
Mathieu Lajugie <mathieu@caring.com>
2015-02-07 20:05:56 -0800

    clean things up

--- app/application.jsx
+++ app/application.jsx
@@ -1,13 +1,9 @@
 import CommentBox from 'components/comment-box';
 
-export default {
-  start: function () {
-    React.render(
-      <div className="application">
-        <h1>Application</h1>
-        <CommentBox />
-      </div>,
-      document.getElementById('content')
-    );
-  }
-};
+React.render(
+  <div className="application">
+    <h1>Application</h1>
+    <CommentBox />
+  </div>,
+  document.getElementById('content')
+);
--- app/boot.js
+++
@@ -1,9 +0,0 @@
-import Tester from './utils/tester';
-import Application from './application';
-
-let joe = new Tester('Joe');
-
-joe.sayHi();
-// joe.alertMe();
-
-Application.start();
